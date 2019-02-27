4d458b298358de90ced8d69e28bc4736d80ed60f
sainttobs <sainttobs@gmail.com>
2019-02-27 02:51:41 +0100

    successfully scraped my site

--- learn_Scrapy/spiders/stories_spider.py
+++ learn_Scrapy/spiders/stories_spider.py
@@ -7,15 +7,21 @@ class StoriesSpider(scrapy.Spider):
      # urls that you want to crawl
       'https://9jatechguru.com.ng/category/smartphone',
       'https://9jatechguru.com.ng/category/technology',
+      'https://9jatechguru.com.ng/category/cryptocurrency',
+      'https://9jatechguru.com.ng/category/internet',
+      'https://9jatechguru.com.ng/category/laptops',
+      'https://9jatechguru.com.ng/category/security',
+      'https://9jatechguru.com.ng/category/businesses',
+      'https://9jatechguru.com.ng/category/blogging',
   ]
 
 # For All Stories
   def parse(self, response):
     # Replace 'path' with the right css path that the data located
-    for story in response.css('path'):
+    for story in response.css('div.content-area'):
       yield {
           # Things you need to crawl
-          'articleTitle': story.css('.entry-title::text').extract(),
-          'articleLink': story.css('.entry-title a::attr(href)').extract(),
-          'postingDate': story.css('.date').extract(),
+          'articleTitle': story.css('h2.entry-title a::text').extract_first(),
+          'articleLink': story.css('h2.entry-title a::attr(href)').extract_first(),
+          'postingDate': story.css('span.posted-on::text').extract_first(),
       }
